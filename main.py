b39f6f4102859ce4ec47837990829095fd02292e
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-14 15:23:59 -0500

    logs on, no functionality

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from CONSTANTS import MY_TOKEN
+from bot_logging.bot_logger import DiscordBotLog
+from bot_memory.bot_brain import BotBrain
+from SECRETS import MY_TOKEN
 import discord
+
+bot_logger = DiscordBotLog()
+bot_brain = BotBrain()
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        bot_brain.announce("logged on as {0}!".format(self.user))
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        bot_brain.announce("message from {0.author}: {0.content}".format(message))
+        if message.author == self.user:
+            bot_logger.info_event("received message from self")
+            return
+
+        if ' ' in message.content:
+            bot_logger.info_event("responding to message")
+            await message.channel.send('i am not to be spoken with')
 
 
 if __name__ == '__main__':
