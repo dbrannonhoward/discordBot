69f30a6ed5341e3c803c78f4343c7dad55904ee2
dbrannonhoward <dbrannonhoward@gmail.com>
2020-11-14 15:22:22 -0500

    idk how to write tests

---
+++ tests/test_bot_brain.py
@@ -0,0 +1,17 @@
+from bot_memory.bot_brain import BotBrain
+from time import sleep
+test_bb = BotBrain()
+
+print("starting tests..")
+
+test_bb.set_last_user_spoken_to("tom_the_tester")
+print(test_bb.get_last_user_spoken_to())
+sleep(1)
+test_bb.set_time_initialized()
+print(test_bb.get_time_initialized())
+sleep(1)
+test_bb.set_time_last_message_sent()
+print(test_bb.get_time_last_message_sent())
+sleep(1)
+test_bb.set_time_last_message_received()
+print(test_bb.get_time_last_message_received())
